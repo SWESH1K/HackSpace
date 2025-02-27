777504d1eaf1e6ab082667807250f91e227b2516
lucky <lucky@luckys-MacBook-Air.local>
2025-02-27 08:57:36 +0530

    temp2

--- frontend/src/pages/RulesRegulations.tsx
+++ frontend/src/pages/RulesRegulations.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import ReactMarkdown from 'react-markdown';
+import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import styles from "../SingleEventPage.module.css";
 
@@ -20,6 +19,6 @@ const RulesRegulations = ({ event }: RulesRegulationsProps) => {
       </ReactMarkdown>
     </div>
   );
-}
+};
 
 export default RulesRegulations;
