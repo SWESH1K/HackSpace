1965949140be42e6a8b92208c4a4b938c8768fd7
lucky <lucky@luckys-MacBook-Air.local>
2025-04-28 13:13:42 +0530

    Added result mssge

--- frontend/src/pages/ResultPage.tsx
+++ frontend/src/pages/ResultPage.tsx
@@ -1,6 +1,8 @@
 const ResultPage = () => {
   return (
-    <div>ResultPage Content</div>
+    <div className="flex justify-center min-h-[60vh] items-start mt-14">
+      <div className="text-3xl font-bold">Results are not yet announced</div>
+    </div>
   )
 }
 
